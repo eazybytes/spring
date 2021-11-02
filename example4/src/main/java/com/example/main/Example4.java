a40a1de18b1f7e289094c66e5b7a0b4907ab117c
Eazy Bytes <tutor@eazybytes.com>
2021-11-02 22:30:21 +0530

    Primary Annotation

---
+++ example4/src/main/java/com/example/main/Example4.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example4 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Primary Vehicle name from Spring Context is: " + vehicle.getName());
+
+    }
+}
