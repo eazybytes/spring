d055ee4c517e085e4bbcb65d77b03d2204918c5a
Eazy Bytes <tutor@eazybytes.com>
2021-11-02 22:02:34 +0530

    Different way to name a Bean

---
+++ example3/src/main/java/com/example/main/Example3.java
@@ -0,0 +1,22 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh1 = context.getBean("audiVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh1.getName());
+
+        Vehicle veh2 = context.getBean("hondaVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh2.getName());
+
+        Vehicle veh3 = context.getBean("ferrariVehicle",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh3.getName());
+    }
+}
