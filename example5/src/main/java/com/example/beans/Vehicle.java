58e08584791fded6df83405d9e91278853bc117e
Eazy Bytes <tutor@eazybytes.com>
2021-11-06 13:44:50 +0530

    Bean creation using Component Annotation

---
+++ example5/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,22 @@
+package com.example.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println(
+            "Printing Hello from Component Vehicle Bean");
+    }
+}
---
+++ example5/src/main/java/com/example/main/Example5.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example5 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName());
+        vehicle.printHello();
+
+    }
+}
