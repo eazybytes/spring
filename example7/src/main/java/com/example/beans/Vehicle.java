7a05f8c09b09cc9b2b3130245fa98ddbbeb0111b
Eazy Bytes <tutor@eazybytes.com>
2021-11-09 10:14:59 +0530

    Programmatically adding beans to the Spring context

---
+++ example7/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,18 @@
+package com.example.beans;
+
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing Hello from Component Vehicle Bean");
+    }
+}
