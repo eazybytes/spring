8c16e321f165fd78b0538d671d8de14eb6484735
Eazy Bytes <tutor@eazybytes.com>
2021-11-28 16:38:22 +0530

    Singleton Bean Scope inside Spring

---
+++ example_14/src/main/java/com/example/implementation/MichelinTyres.java
@@ -0,0 +1,15 @@
+package com.example.implementation;
+
+import com.example.interfaces.Tyres;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class MichelinTyres implements Tyres {
+
+    public String rotate(){
+        return "Vehicle moving with Michelin tyres";
+    }
+
+}
