b9faf5f7a130597db525da180dda6b1a35df30ac
Eazy Bytes <tutor@eazybytes.com>
2021-12-16 19:51:39 +0530

    Hello World Web App using SpringBoot & Spring MVC

---
+++ example_18/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage() {
+        return "home.html";
+    }
+
+}
