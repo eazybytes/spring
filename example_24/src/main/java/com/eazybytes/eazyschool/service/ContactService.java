166406f580ae8581fc7c1e4308e6d3ddbdc6a2ca
Eazy Bytes <tutor@eazybytes.com>
2021-12-18 19:02:32 +0530

    Validating the form input using Java Bean & Hibernate Validators

---
+++ example_24/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -0,0 +1,27 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+public class ContactService {
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+}
