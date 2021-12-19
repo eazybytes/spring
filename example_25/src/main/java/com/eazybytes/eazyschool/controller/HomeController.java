31ebc133c47f580755c581619c799521d990fc6d
Eazy Bytes <tutor@eazybytes.com>
2021-12-19 14:46:38 +0530

    Demo of Spring Bean Web Scopes

---
+++ example_25/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage() {
+        return "home.html";
+    }
+
+}
