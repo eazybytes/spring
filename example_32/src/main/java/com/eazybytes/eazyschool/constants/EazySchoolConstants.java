d05d7aaa3e78aea7b7b175462536d9f033e3d966
Eazy Bytes <tutor@eazybytes.com>
2021-12-23 22:05:25 +0530

    Introduction to JDBCTemplate, H2 DB, Store Contact Msgs in DB

---
+++ example_32/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,8 @@
+package com.eazybytes.eazyschool.constants;
+
+public interface EazySchoolConstants {
+
+    public static final String ANONYMOUS = "Anonymous";
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+}
---
+++ example_32/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -0,0 +1,47 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Holiday;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+public class HolidaysController {
+
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display,Model model) {
+        if(null != display && display.equals("all")){
+            model.addAttribute("festival",true);
+            model.addAttribute("federal",true);
+        }else if(null != display && display.equals("federal")){
+            model.addAttribute("federal",true);
+        }else if(null != display && display.equals("festival")){
+            model.addAttribute("festival",true);
+        }
+        List<Holiday> holidays = Arrays.asList(
+                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+        );
+        Holiday.Type[] types = Holiday.Type.values();
+        for (Holiday.Type type : types) {
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+        }
+        return "holidays.html";
+    }
+
+}
