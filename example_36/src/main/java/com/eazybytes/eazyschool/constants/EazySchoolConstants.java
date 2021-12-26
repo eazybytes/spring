993074450c82ac603806d9a3e3cfa0c6c1f6db62
Eazy Bytes <tutor@eazybytes.com>
2021-12-26 12:24:29 +0530

    Spring JPA Auditing using @CreatedBy,@LastModifiedBy,@CreatedDate,@LastModifiedDate

---
+++ example_36/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,7 @@
+package com.eazybytes.eazyschool.constants;
+
+public interface EazySchoolConstants {
+
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+}
---
+++ example_36/src/main/resources/eazyschool_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 1 ','New Year''s Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Oct 31 ','Halloween','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Nov 24 ','Thanksgiving Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Dec 25 ','Christmas','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 17 ','Martin Luther King Jr. Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' July 4 ','Independence Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Sep 5 ','Labor Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+  VALUES (' Nov 11 ','Veterans Day','FEDERAL',CURDATE(),'DBA');
