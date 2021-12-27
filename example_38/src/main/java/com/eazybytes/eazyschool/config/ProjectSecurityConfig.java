61dbf14297736e5e72d79a8627e6d10b10a26aa8
Eazy Bytes <tutor@eazybytes.com>
2021-12-27 11:37:05 +0530

    Authentication with credentials in DB using JPA

---
+++ example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+            http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/public/**").and()
+                .authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
+                .mvcMatchers("/displayMessages").hasRole("ADMIN")
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/public/**").permitAll()
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().httpBasic();
+    }
+
+}
---
+++ example_38/src/main/java/com/eazybytes/eazyschool/controller/PublicController.java
@@ -0,0 +1,47 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("public")
+public class PublicController {
+
+    @Autowired
+    PersonService personService;
+
+    @RequestMapping(value ="/register",method = { RequestMethod.GET})
+    public String displayRegisterPage(Model model) {
+        model.addAttribute("person", new Person());
+        return "register.html";
+    }
+
+    @RequestMapping(value ="/createUser",method = { RequestMethod.POST})
+    public String createUser(@Valid @ModelAttribute("person") Person person, Errors errors) {
+        if(errors.hasErrors()){
+            return "register.html";
+        }
+        boolean isSaved = personService.createNewPerson(person);
+        if(isSaved){
+            return "redirect:/login?register=true";
+        }else {
+            return "register.html";
+        }
+    }
+
+}
