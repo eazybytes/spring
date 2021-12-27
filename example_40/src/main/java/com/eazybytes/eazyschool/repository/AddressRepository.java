5323dac8bf09b74724c0e01864034b81ea17b69a
Eazy Bytes <tutor@eazybytes.com>
2021-12-27 20:47:53 +0530

    Profile & Address Update inside Eazy School App

---
+++ example_40/src/main/java/com/eazybytes/eazyschool/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+}
---
+++ example_40/src/main/java/com/eazybytes/eazyschool/service/PersonService.java
@@ -0,0 +1,39 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.constants.EazySchoolConstants;
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Roles;
+import com.eazybytes.eazyschool.repository.ContactRepository;
+import com.eazybytes.eazyschool.repository.PersonRepository;
+import com.eazybytes.eazyschool.repository.RolesRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean createNewPerson(Person person){
+        boolean isSaved = false;
+        Roles role = rolesRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
+        person = personRepository.save(person);
+        if (null != person && person.getPersonId() > 0)
+        {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+}
