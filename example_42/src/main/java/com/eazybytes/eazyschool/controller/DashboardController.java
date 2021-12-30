d423c94b4a33fa9241ddb2014c9ef90562c71b41
Eazy Bytes <tutor@eazybytes.com>
2021-12-30 13:49:34 +0530

    ManyToMany mapping demo using Courses, Students

--- example_42/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ example_42/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -29,6 +29,9 @@ public class DashboardController {
         Person person = personRepository.readByEmail(authentication.getName());
         model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        if(null != person.getEazyClass() && null != person.getEazyClass().getName()){
+            model.addAttribute("enrolledClass", person.getEazyClass().getName());
+        }
         session.setAttribute("loggedInPerson", person);
         return "dashboard.html";
     }
