2d914ef6c1468887275466ae3014c9e9529c953f
Eazy Bytes <tutor@eazybytes.com>
2022-01-05 16:39:47 +0530

    Exploring Actuator Info using SpringBoot Admin UI

---
+++ example_51/src/main/java/com/eazybytes/eazyschool/EazySchoolAdminApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eazyschool;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class EazySchoolAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolAdminApplication.class, args);
+	}
+
+}
---
+++ example_51/src/test/java/com/eazybytes/eazyschool/Example51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
