d76c8ec987c37e56f756ce0a7feed3a5a588b5db
Eazy Bytes <tutor@eazybytes.com>
2022-01-21 17:38:24 +0530

    Demo of Properties Configuration & Spring Profiles

--- example_49/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ example_49/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -3,7 +3,6 @@ package com.eazybytes.eazyschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
