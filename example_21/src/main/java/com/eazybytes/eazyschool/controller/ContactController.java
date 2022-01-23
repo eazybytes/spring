fb381c4f64b6ebb58dfc98a954c364cc2ff28fcb
Eazy Bytes <tutor@eazybytes.com>
2022-01-23 21:12:44 +0530

    Build EazySchool Courses, About, Contact Pages & Intro to Lombok

--- example_21/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_21/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -39,9 +38,9 @@ public class ContactController {
     }*/
 
     @RequestMapping(value = "/saveMsg",method = POST)
-    public String saveMessage(Contact contact) {
+    public ModelAndView saveMessage(Contact contact){
         contactService.saveMessageDetails(contact);
-        return "redirect:/contact";
+        return new ModelAndView("redirect:/contact");
     }
 
 
--- example_21/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
+++ example_21/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -5,11 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
