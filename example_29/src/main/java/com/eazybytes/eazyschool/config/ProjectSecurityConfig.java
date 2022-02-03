cc1a4cd35401d7de28021a9bd6367dd55ef63553
Eazy Bytes <tutor@eazybytes.com>
2022-02-03 19:07:19 +0530

    Securing Web Application with InMemoryAuthentication & Custom Login Page

--- example_29/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_29/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,14 +1,10 @@
 package com.eazybytes.eazyschool.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,10 +32,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("user").password("12345").roles("USER")
-            .and()
-            .withUser("admin").password("54321").roles("USER", "ADMIN")
-            .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("54321").roles("USER", "ADMIN")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 }
