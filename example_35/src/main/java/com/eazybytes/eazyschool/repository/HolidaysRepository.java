715bbf8584ae5e1705465abffb77eec68e8bb512
Eazy Bytes <tutor@eazybytes.com>
2022-02-11 18:50:47 +0530

    Migrate from JDBCTemplate to Spring Data JPA

--- example_35/src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
+++ example_35/src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
@@ -1,13 +1,8 @@
 package com.eazybytes.eazyschool.repository;
 
 import com.eazybytes.eazyschool.model.Holiday;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /*
 @Repository stereotype annotation is used to add a bean of this class
