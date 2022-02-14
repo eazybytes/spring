08261bc6b452a6050bababc895fd123743b83a2e
Eazy Bytes <tutor@eazybytes.com>
2022-02-14 16:13:48 +0530

    Authentication with credentials in DB using JPA

--- example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.eazybytes.eazyschool.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
