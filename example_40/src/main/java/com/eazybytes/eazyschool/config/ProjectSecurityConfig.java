9cc87f79d9268172924e9a757b9b4d29c67d6b38
eazybytes <tutor@eazybytes.com>
2022-08-08 21:20:45 +0530

    Profile & Address Update inside Eazy School App

--- example_40/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_40/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -3,7 +3,6 @@ package com.eazybytes.eazyschool.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
