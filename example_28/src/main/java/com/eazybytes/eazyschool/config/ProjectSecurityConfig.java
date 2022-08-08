1806d7fc4f7cbd1f5c1bb413bb17285b365de7a8
eazybytes <tutor@eazybytes.com>
2022-08-08 15:04:27 +0530

    Custom Security Configurations for public pages & CSRF Disable

--- example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,25 +1,34 @@
 package com.eazybytes.eazyschool.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ProjectSecurityConfig  {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-            http.csrf().disable()
+    /**
+     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+     * to move towards a component-based security configuration. It is recommended to create a bean
+     * of type SecurityFilterChain for security related configurations.
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
                 .authorizeRequests()
                 .mvcMatchers("/home").permitAll()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
-                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/courses").authenticated()
                 .mvcMatchers("/about").permitAll()
                 .and().formLogin().and().httpBasic();
-
+        return http.build();
     }
 
 }
