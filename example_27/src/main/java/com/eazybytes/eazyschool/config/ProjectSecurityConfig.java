212575f19f804122e409f8549b06c3e53b76105e
eazybytes <tutor@eazybytes.com>
2022-08-08 14:50:47 +0530

    Configure PermitAll & DenyAll using Spring Security

--- example_27/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_27/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.eazybytes.eazyschool.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ProjectSecurityConfig  /*extends WebSecurityConfigurerAdapter*/  {
 
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
 
         // Permit All Requests inside the Web Application
@@ -17,10 +19,35 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and().httpBasic();
 
         // Deny All Requests inside the Web Application
-        /*http.authorizeRequests().
+        *//*http.authorizeRequests().
                 anyRequest().denyAll().
                 and().formLogin()
-                .and().httpBasic();*/
+                .and().httpBasic();*//*
+    }*/
+
+    /**
+     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+     * to move towards a component-based security configuration. It is recommended to create a bean
+     * of type SecurityFilterChain for security related configurations.
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // Permit All Requests inside the Web Application
+            http.authorizeRequests().anyRequest().permitAll().
+                    and().formLogin()
+                    .and().httpBasic();
+
+        // Deny All Requests inside the Web Application
+            /*http.authorizeRequests().anyRequest().denyAll().
+                    and().formLogin()
+                    .and().httpBasic();*/
+        
+            return http.build();
+
     }
 
 }
