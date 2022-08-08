2e879f8ad2518a6db50bcb735739f4ad210cbbde
eazybytes <tutor@eazybytes.com>
2022-08-08 15:45:50 +0530

    Configure PermitAll & DenyAll using Spring Security

--- example_27/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_27/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -3,7 +3,6 @@ package com.eazybytes.eazyschool.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
