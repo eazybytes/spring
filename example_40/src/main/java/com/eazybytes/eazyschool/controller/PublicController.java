5079026e4a5fbacec78ddc7ea10d95c3e4b51900
eazybytes <tutor@eazybytes.com>
2022-12-06 21:08:04 +0530

    Profile & Address Update inside Eazy School App

--- example_40/src/main/java/com/eazybytes/eazyschool/controller/PublicController.java
+++ example_40/src/main/java/com/eazybytes/eazyschool/controller/PublicController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @Slf4j
 @Controller
