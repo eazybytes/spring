5ad6561b6a126509d3df2f41f888b737e942e29f
eazybytes <tutor@eazybytes.com>
2022-12-06 11:46:50 +0530

    Introduction to JDBCTemplate, H2 DB, Store Contact Msgs in DB

--- example_32/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_32/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -26,6 +26,7 @@ public class ProjectSecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/displayMessages").hasRole("ADMIN")
+                .requestMatchers("/closeMsg/**").hasRole("ADMIN")
                 .requestMatchers("/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
