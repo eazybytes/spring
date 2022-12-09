cf7971e246025f4253d261a14a44d0181e7cc4d6
eazybytes <tutor@eazybytes.com>
2022-12-09 21:44:47 +0530

    Demo of Query, Modifying, Transactional, NamedQuery, NamedNativeQueries

--- example_44/src/main/java/com/eazybytes/eazyschool/controller/StudentController.java
+++ example_44/src/main/java/com/eazybytes/eazyschool/controller/StudentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
--- example_44/src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
+++ example_44/src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.Set;
 
 @Getter
