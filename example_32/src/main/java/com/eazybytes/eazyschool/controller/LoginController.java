2ba67a97673d14780a411ce820da594abde6afec
eazybytes <tutor@eazybytes.com>
2023-05-14 16:16:26 +0530

    Introduction to JDBCTemplate, H2 DB, Store Contact Msgs in DB

--- example_32/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
+++ example_32/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.eazybytes.eazyschool.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,21 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 @Slf4j
 @Controller
 public class LoginController {
 
-    @RequestMapping(value ="/login",method = { RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
     public String displayLoginPage(@RequestParam(value = "error", required = false) String error,
-                                   @RequestParam(value = "logout", required = false) String logout,Model model) {
+                                   @RequestParam(value = "logout", required = false) String logout, Model model) {
         String errorMessge = null;
-        if(error != null) {
+        if (error != null) {
             errorMessge = "Username or Password is incorrect !!";
         }
-        if(logout != null) {
+        if (logout != null) {
             errorMessge = "You have been successfully logged out !!";
         }
         model.addAttribute("errorMessge", errorMessge);
@@ -39,7 +38,5 @@ public class LoginController {
         }
         return "redirect:/login?logout=true";
     }
-
-
 
 }
