e7f05a47884e9e6b093592862066a1310e38c6a7
eazybytes <tutor@eazybytes.com>
2023-05-14 20:59:36 +0530

    Password hashing using BCryptPasswordEncoder

--- example_39/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_39/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.validation.Valid;
 
 import java.util.List;
 
