a51435fd52d6e99f9f420ba58c54e059fc7ac538
eazybytes <tutor@eazybytes.com>
2023-05-14 12:52:21 +0530

    Securing Web Application with InMemory users & Custom Login Page

--- example_29/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
+++ example_29/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
@@ -1,17 +1,11 @@
 package com.eazybytes.eazyschool.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
