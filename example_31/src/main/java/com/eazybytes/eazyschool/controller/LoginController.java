f531b12ce9b5ecb4fdddce18d79dd79aa323484e
eazybytes <tutor@eazybytes.com>
2023-05-14 16:08:12 +0530

    Fix for CSRF Attacks using CSRF tokens

--- example_31/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
+++ example_31/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.eazybytes.eazyschool.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,15 @@ public class LoginController {
         }
         model.addAttribute("errorMessge", errorMessge);
         return "login.html";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
     }
 
 }
