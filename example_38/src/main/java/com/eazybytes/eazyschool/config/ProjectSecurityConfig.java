6d4814cfe5f0b0159170d245262188df66b2383e
eazybytes <tutor@eazybytes.com>
2023-05-14 20:49:35 +0530

    Authentication with credentials in DB using JPA

--- example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -8,15 +8,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
 
-    /**
-     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
-     * to move towards a component-based security configuration. It is recommended to create a bean
-     * of type SecurityFilterChain for security related configurations.
-     *
-     * @param http
-     * @return SecurityFilterChain
-     * @throws Exception
-     */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
@@ -25,22 +16,22 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/displayMessages").hasRole("ADMIN")
                 .requestMatchers("/closeMsg/**").hasRole("ADMIN")
-                .requestMatchers("/home").permitAll()
+                .requestMatchers("", "/", "/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/saveMsg").permitAll()
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
                 .requestMatchers("/login").permitAll()
                 .requestMatchers("/logout").permitAll()
-                .requestMatchers("/assets/**").permitAll()
                 .requestMatchers("/public/**").permitAll()
                 .and().formLogin().loginPage("/login")
                 .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
                 .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
                 .and().httpBasic();
 
-            return http.build();
+        return http.build();
     }
 
 }
--- example_38/src/main/java/com/eazybytes/eazyschool/repository/RolesRepository.java
+++ example_38/src/main/java/com/eazybytes/eazyschool/repository/RolesRepository.java
@@ -1,11 +1,8 @@
 package com.eazybytes.eazyschool.repository;
 
-import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface RolesRepository extends JpaRepository<Roles, Integer> {
