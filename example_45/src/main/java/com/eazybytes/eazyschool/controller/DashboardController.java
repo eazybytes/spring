e883e44cec36e5a3b0047dc691d2348accc0273b
eazybytes <tutor@eazybytes.com>
2023-05-15 14:44:43 +0530

    Implementing Rest Services

--- example_45/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ example_45/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -2,20 +2,13 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
