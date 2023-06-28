92359e43d0f600d8b6004e8562444921b1350af4
eazybytes <tutor@eazybytes.com>
2023-06-28 18:00:53 +0530

    Custom Security Configurations for public pages & CSRF Disable

--- example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_28/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,16 +11,16 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("", "/", "/home").permitAll()
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("", "/", "/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/saveMsg").permitAll()
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
-                .requestMatchers("/assets/**").permitAll()
-                .and().formLogin().and().httpBasic();
+                .requestMatchers("/assets/**").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
