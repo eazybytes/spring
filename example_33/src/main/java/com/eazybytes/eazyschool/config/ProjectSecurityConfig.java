318fd0bccdd2822cc98708fd020f36d22e403a38
eazybytes <tutor@eazybytes.com>
2023-08-06 16:23:18 +0530

    Display Holidays from DB & Demo of BeanPropertyRowMapper

--- example_33/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_33/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -9,26 +9,33 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg").ignoringRequestMatchers(PathRequest.toH2Console()))
-                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
-                    .requestMatchers("/displayMessages").hasRole("ADMIN")
-                    .requestMatchers("/closeMsg/**").hasRole("ADMIN")
-                    .requestMatchers("", "/", "/home").permitAll()
-                    .requestMatchers("/holidays/**").permitAll()
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/saveMsg").permitAll()
-                    .requestMatchers("/courses").permitAll()
-                    .requestMatchers("/about").permitAll()
-                    .requestMatchers("/assets/**").permitAll()
-                    .requestMatchers("/login").permitAll()
-                    .requestMatchers("/logout").permitAll()
-                    .requestMatchers(PathRequest.toH2Console()).permitAll())
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers(mvcMatcherBuilder.pattern("/saveMsg"))
+                        .ignoringRequestMatchers(PathRequest.toH2Console()))
+                .authorizeHttpRequests((requests) -> requests.requestMatchers(mvcMatcherBuilder.pattern("/dashboard")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/displayMessages")).hasRole("ADMIN")
+                        .requestMatchers(mvcMatcherBuilder.pattern("/closeMsg/**")).hasRole("ADMIN")
+                        .requestMatchers(mvcMatcherBuilder.pattern("")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/home")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/holidays/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/contact")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/saveMsg")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/courses")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/about")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/assets/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/login")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/logout")).permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll())
                 .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
                         .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
                 .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
