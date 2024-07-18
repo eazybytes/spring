3d1e7fc543b90e0576621b93994b9d2bb3017bab
eazybytes <tutor@eazybytes.com>
2024-07-18 11:23:49 +0530

    Hello World Web App using SpringBoot & Spring MVC

--- example_18/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ example_18/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 public class EazyschoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EazyschoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EazyschoolApplication.class, args);
+    }
 
 }
