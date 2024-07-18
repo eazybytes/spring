eb064c16f32730bc5184e1c5abaced5fd88ffc29
eazybytes <tutor@eazybytes.com>
2024-07-18 15:20:42 +0530

    Implement Error page using ControllerAdvice annotation

--- example_30/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_30/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -17,7 +17,7 @@ public class ProjectSecurityConfig {
 
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
-                .requestMatchers("", "/", "/home").permitAll()
+                .requestMatchers("/", "/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/saveMsg").permitAll()
