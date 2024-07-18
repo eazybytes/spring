53e7ee08383922cdd7fb333dbb6e356190eb328c
eazybytes <tutor@eazybytes.com>
2024-07-18 20:13:11 +0530

    ManyToMany mapping demo using Courses, Students

--- example_42/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_42/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -22,7 +22,7 @@ public class ProjectSecurityConfig {
                     .requestMatchers("/displayProfile").authenticated()
                     .requestMatchers("/updateProfile").authenticated()
                     .requestMatchers("/student/**").hasRole("STUDENT")
-                    .requestMatchers("", "/", "/home").permitAll()
+                    .requestMatchers("/", "/home").permitAll()
                     .requestMatchers("/holidays/**").permitAll()
                     .requestMatchers("/contact").permitAll()
                     .requestMatchers("/saveMsg").permitAll()
