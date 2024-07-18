8c2a33c8989763a9aa3858b19450929bbefeb46c
eazybytes <tutor@eazybytes.com>
2024-07-18 19:23:31 +0530

    New Registration, Custom Validations,OnetoOne Mapping Demo

--- example_37/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_37/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -19,7 +19,7 @@ public class ProjectSecurityConfig {
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
                     .requestMatchers("/displayMessages").hasRole("ADMIN")
                     .requestMatchers("/closeMsg/**").hasRole("ADMIN")
-                    .requestMatchers("", "/", "/home").permitAll()
+                    .requestMatchers("/", "/home").permitAll()
                     .requestMatchers("/holidays/**").permitAll()
                     .requestMatchers("/contact").permitAll()
                     .requestMatchers("/saveMsg").permitAll()
