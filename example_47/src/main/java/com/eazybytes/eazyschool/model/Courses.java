b086b2b9fb4e1eb8e532c5e1ba9f763f1b20bf2c
eazybytes <tutor@eazybytes.com>
2025-06-05 10:51:23 +0530

    Spring Data Rest & HAL Explorer changes

--- example_47/src/main/java/com/eazybytes/eazyschool/model/Courses.java
+++ example_47/src/main/java/com/eazybytes/eazyschool/model/Courses.java
@@ -14,8 +14,7 @@ import java.util.Set;
 public class Courses extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native",strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int courseId;
 
     private String name;
