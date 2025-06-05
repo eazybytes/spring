b44191834c8fa71abff798998e1e58f1c23ed458
eazybytes <tutor@eazybytes.com>
2025-06-05 10:21:29 +0530

    Migrate from JDBCTemplate to Spring Data JPA

--- example_35/src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ example_35/src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 /*
 @Data annotation is provided by Lombok library which generates getter, setter,
@@ -15,12 +14,11 @@ This makes our code short and clean.
 * */
 @Data
 @Entity
-@Table(name="contact_msg")
-public class Contact extends BaseEntity{
+@Table(name = "contact_msg")
+public class Contact extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native",strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "contact_id")
     private int contactId;
 
@@ -29,24 +27,24 @@ public class Contact extends BaseEntity{
       @NotEmpty: Checks if a given field is not null and its size/length is greater than zero.
       @NotBlank: Checks if a given field is not null and trimmed length is greater than zero.
     * */
-    @NotBlank(message="Name must not be blank")
-    @Size(min=3, message="Name must be at least 3 characters long")
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
 
-    @NotBlank(message="Mobile number must not be blank")
-    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNum;
 
-    @NotBlank(message="Email must not be blank")
-    @Email(message = "Please provide a valid email address" )
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
-    @NotBlank(message="Subject must not be blank")
-    @Size(min=5, message="Subject must be at least 5 characters long")
+    @NotBlank(message = "Subject must not be blank")
+    @Size(min = 5, message = "Subject must be at least 5 characters long")
     private String subject;
 
-    @NotBlank(message="Message must not be blank")
-    @Size(min=10, message="Message must be at least 10 characters long")
+    @NotBlank(message = "Message must not be blank")
+    @Size(min = 10, message = "Message must be at least 10 characters long")
     private String message;
 
     private String status;
