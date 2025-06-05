320e851c58adf377ec12eb28fb91f2cc0968d362
eazybytes <tutor@eazybytes.com>
2025-06-05 10:52:00 +0530

    Understand & Configuring logging inside SpringBoot

--- example_48/src/main/java/com/eazybytes/eazyschool/model/Person.java
+++ example_48/src/main/java/com/eazybytes/eazyschool/model/Person.java
@@ -33,8 +33,7 @@ import java.util.Set;
 public class Person extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native",strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int personId;
 
     @NotBlank(message="Name must not be blank")
