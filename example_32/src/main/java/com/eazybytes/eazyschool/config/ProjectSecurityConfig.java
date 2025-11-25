b006c8a423df12ca45b1f34952ab563618bdfe22
eazybytes <tutor@eazybytes.com>
2025-11-25 21:11:00 +0530

    Intro to JDBCTemplate & Store Contact Msgs in H2 DB

--- example_32/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_32/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.eazybytes.eazyschool.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
