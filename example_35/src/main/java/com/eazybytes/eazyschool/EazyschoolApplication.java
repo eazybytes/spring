8f4a56796816e922ff9190d1ce65a8d5c1337dd5
eazybytes <tutor@eazybytes.com>
2025-11-26 10:19:55 +0530

    Migrate from JDBCTemplate to Spring Data JPA

--- example_35/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ example_35/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -2,7 +2,7 @@ package com.eazybytes.eazyschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
