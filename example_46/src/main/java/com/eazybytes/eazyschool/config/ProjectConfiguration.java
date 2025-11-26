f8518220709d5a32501124e9c6a506ff3be2d824
eazybytes <tutor@eazybytes.com>
2025-11-26 16:20:13 +0530

    Consuming Rest Services using FeignClient,WebClient & RestTemplate

--- example_46/src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
+++ example_46/src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
@@ -1,7 +1,7 @@
 package com.eazybytes.eazyschool.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
