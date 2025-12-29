ff5d38032bb623d8f6cc9bdcb797d40fa0b9478f
eazybytes <tutor@eazybytes.com>
2025-12-29 17:50:06 +0530

    Mastering Transactions in Spring Data JPA – From Basics to Real Projects

--- section_17/jobportal/src/main/java/com/eazybytes/jobportal/user/controller/UserController.java
+++ section_17/jobportal/src/main/java/com/eazybytes/jobportal/user/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
